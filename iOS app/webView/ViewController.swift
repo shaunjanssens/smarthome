6339a9114ed7a6189908651c479987b74a08542a
Shaun Janssens <shaunjanssens@gmail.com>
2018-01-04 23:10:39 +0100

    Add basic iOS app

---
+++ iOS app/webView/ViewController.swift
@@ -0,0 +1,30 @@
+//
+//  ViewController.swift
+//  webView
+//
+//  Created by Yash Patel on 18/10/17.
+//  Copyright © 2017 Yash Patel. All rights reserved.
+//
+
+import UIKit
+import WebKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var webview: WKWebView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        let url = URL(string: "https://smarthome-9d4f6.firebaseapp.com/")
+        let request = URLRequest(url: url!)
+        
+        webview.load(request)
+    }
+    
+    override var prefersStatusBarHidden: Bool {
+        return true
+    }
+
+}
+
